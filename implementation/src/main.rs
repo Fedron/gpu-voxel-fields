1a0287137aa748738566e95370050a3bc74118fe
Nicolas Fedor <fedronn@gmail.com>
2024-10-04 11:54:43 +0100

    refactor(impl): move app logic to separate module

--- implementation/src/main.rs
+++ implementation/src/main.rs
@@ -1,69 +1,87 @@
-use glium::{uniform, Surface};
+use std::rc::Rc;
+
+use app::{App, AppBehaviour, Window};
+use glium::{program::ComputeShader, uniform, Surface, Texture2d};
+use winit::event::{Event, WindowEvent};
+
+mod app;
+
+const INITIAL_WINDOW_WIDTH: u32 = 1280;
+const INITIAL_WINDOW_HEIGHT: u32 = 720;
+
+struct VoxelApp {
+    window: Rc<Window>,
+
+    raymarched_texture: Texture2d,
+    raymarcher: ComputeShader,
+}
+
+impl VoxelApp {
+    fn new(window: Rc<Window>) -> Self {
+        let display = &window.display;
+        let raymarched_texture =
+            Texture2d::empty(display, INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT)
+                .expect("to create texture");
+        let raymarcher =
+            ComputeShader::from_source(display, include_str!("shaders/raymarcher.comp"))
+                .expect("to create compute shader");
+
+        Self {
+            window,
+
+            raymarched_texture,
+            raymarcher,
+        }
+    }
+}
+
+impl AppBehaviour for VoxelApp {
+    fn process_events(&mut self, event: winit::event::Event<()>) -> bool {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::Resized(new_size),
+                ..
+            } => {
+                self.raymarched_texture =
+                    Texture2d::empty(&self.window.display, new_size.width, new_size.height)
+                        .expect("to create texture");
+            }
+            _ => {}
+        }
+
+        true
+    }
+
+    fn update(&mut self, _delta_time: std::time::Duration) {}
+
+    fn render(&mut self, frame: &mut glium::Frame) {
+        let out_image = self
+            .raymarched_texture
+            .image_unit(glium::uniforms::ImageUnitFormat::RGBA8)
+            .expect("to create image unit")
+            .set_access(glium::uniforms::ImageUnitAccess::Write);
+        self.raymarcher.execute(
+            uniform! {
+                out_image: out_image,
+            },
+            1280,
+            720,
+            1,
+        );
+
+        self.raymarched_texture
+            .as_surface()
+            .fill(frame, glium::uniforms::MagnifySamplerFilter::Nearest);
+    }
+}
 
 fn main() {
-    let event_loop = glium::winit::event_loop::EventLoop::builder()
-        .build()
-        .expect("event loop to be built");
+    let mut app = App::new(
+        "Dynamic scene with an SVDAG",
+        INITIAL_WINDOW_WIDTH,
+        INITIAL_WINDOW_HEIGHT,
+    );
 
-    let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
-        .with_title("Dynamic Scene with an SVDAG")
-        .with_inner_size(1280, 720)
-        .build(&event_loop);
-
-    let raymarched_texture = glium::Texture2d::empty_with_format(
-        &display,
-        glium::texture::UncompressedFloatFormat::U8U8U8U8,
-        glium::texture::MipmapsOption::NoMipmap,
-        1280,
-        720,
-    )
-    .expect("to create texture");
-
-    let raymarcher = glium::program::ComputeShader::from_source(
-        &display,
-        include_str!("shaders/raymarcher.comp"),
-    )
-    .expect("to create ray marcher compute shader");
-
-    #[allow(deprecated)]
-    event_loop
-        .run(move |event, window_target| {
-            match event {
-                glium::winit::event::Event::WindowEvent { event, .. } => match event {
-                    glium::winit::event::WindowEvent::CloseRequested => window_target.exit(),
-                    glium::winit::event::WindowEvent::RedrawRequested => {
-                        let mut frame = display.draw();
-                        frame.clear_color(1.0, 0.0, 1.0, 1.0);
-
-                        let out_image = raymarched_texture
-                            .image_unit(glium::uniforms::ImageUnitFormat::RGBA8)
-                            .expect("to create image unit")
-                            .set_access(glium::uniforms::ImageUnitAccess::Write);
-                        raymarcher.execute(
-                            uniform! {
-                                out_image: out_image,
-                            },
-                            1280,
-                            720,
-                            1,
-                        );
-
-                        raymarched_texture
-                            .as_surface()
-                            .fill(&frame, glium::uniforms::MagnifySamplerFilter::Nearest);
-
-                        frame.finish().expect("to finish drawing");
-                    }
-                    glium::winit::event::WindowEvent::Resized(window_size) => {
-                        display.resize(window_size.into());
-                    }
-                    _ => (),
-                },
-                glium::winit::event::Event::AboutToWait => {
-                    window.request_redraw();
-                }
-                _ => (),
-            };
-        })
-        .unwrap();
+    let voxel_app = VoxelApp::new(app.window.clone());
+    app.run(voxel_app);
 }
